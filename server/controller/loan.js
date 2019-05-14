25cbd08790761d89de6d6929b8853f7887e3106c
okobaba <okobaba2013@gmail.com>
2019-05-14 11:46:37 +0100

    bg:Feedback Resolve [Finishes #165990741]

--- server/controller/loan.js
+++ server/controller/loan.js
@@ -69,8 +69,8 @@ class Loan {
         },
       });
     }
-    return res.status(200).json({
-      status: 404,
+    return res.status(401).json({
+      status: 401,
       error: 'You still owe some money',
     });
   }
@@ -102,8 +102,8 @@ class Loan {
           data,
         });
       }
-      return res.status(400).json({
-        status: 400,
+      return res.status(401).json({
+        status: 401,
         error: 'Pay up your debt',
       });
     }

ac3363e865981d678c83166162eeecdd5f8cb665
okobaba <okobaba2013@gmail.com>
2019-05-22 17:02:43 +0100

    feature: View loans Endpoint [Finishes #166182904]

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import Users from '../controller/users';
-import Loan from '../controller/loan';
+import Loans from '../controller/loans';
 import valid from '../middleware/valid';
 import { verifyUser, verifyAdmin, verifySuperAdmin } from '../middleware/jwt';
 
@@ -12,8 +12,8 @@ const router = express.Router();
 router.post('/auth/signup', validator, validationHandler, Users.create);
 router.post('/auth/signin', Users.login);
 router.patch('/users/:email/verify', verifyAdmin, Users.verify);
-router.get('/loans/:id', verifyAdmin, Loan.specific);
-// router.get('/loans', verifyAdmin, Loan.viewLoans);
+router.get('/loans/:id', verifyAdmin, Loans.specific);
+router.get('/loans', verifyAdmin, Loans.viewLoans);
 // router.get('/loans/:id/repayments', Loan.loanRepayment);
 // router.post('/loans', applyForLoan, validationHandler, verifyUser, Loan.loanApply);
 // router.patch('/loans/:id', verifyAdmin, Loan.Approveloan);

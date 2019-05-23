7ae04ee53128052c3bdc8a889b209cd946ab7bf3
okobaba <okobaba2013@gmail.com>
2019-05-23 16:42:28 +0100

    fix travis [finishes #166205051]

--- dist/middleware/valid.js
+++ dist/middleware/valid.js
@@ -10,7 +10,7 @@ var _check = require("express-validator/check");
 var validator = [(0, _check.check)('firstName').not().isEmpty().withMessage('First name field cannot be empty.'), (0, _check.check)('lastName').not().isEmpty().withMessage('Last name field cannot be empty.'), (0, _check.check)('email').not().isEmpty().withMessage('Email field cannot be empty'), (0, _check.check)('email').isEmail().withMessage('Enter valid email address.'), (0, _check.check)('address').not().isEmpty().withMessage('Address cannot be empty.'), (0, _check.check)('password').not().isEmpty().withMessage('Please password is required'), (0, _check.check)('password').isLength({
   min: 6
 }).withMessage('Password should be atleast 6 characters')];
-var applyForLoan = [(0, _check.check)('amount').not().isEmpty().withMessage('First name field cannot be empty.'), (0, _check.check)('tenor').not().isEmpty().withMessage('Last name field cannot be empty.'), (0, _check.check)('tenor').isInt().withMessage('Please input your tenor in digits')];
+var applyForLoan = [(0, _check.check)('amount').not().isEmpty().withMessage('Amount field cannot be empty.'), (0, _check.check)('tenor').not().isEmpty().withMessage('Kindly input Tenor.'), (0, _check.check)('tenor').isInt().withMessage('Please input your tenor in digits')];
 
 var validationHandler = function validationHandler(req, res, next) {
   var errors = (0, _check.validationResult)(req);
--- dist/routes/routes.js
+++ dist/routes/routes.js
@@ -11,7 +11,7 @@ var _express = _interopRequireDefault(require("express"));
 
 var _users = _interopRequireDefault(require("../controller/users"));
 
-var _loan = _interopRequireDefault(require("../controller/loan"));
+var _loans = _interopRequireDefault(require("../controller/loans"));
 
 var _valid = _interopRequireDefault(require("../middleware/valid"));
 
@@ -25,13 +25,13 @@ var router = _express["default"].Router();
 
 router.post('/auth/signup', validator, validationHandler, _users["default"].create);
 router.post('/auth/signin', _users["default"].login);
-router.patch('/users/:email/verify', _jwt.verifyAdmin, _users["default"].verify); // router.get('/loans/:id', verifyAdmin, Loan.specific);
-// router.get('/loans', verifyAdmin, Loan.viewLoans);
-// router.get('/loans/:id/repayments', Loan.loanRepayment);
-// router.post('/loans', applyForLoan, validationHandler, verifyUser, Loan.loanApply);
-// router.patch('/loans/:id', verifyAdmin, Loan.Approveloan);
-// router.post('/loans/:id/repayment', verifyAdmin, Loan.repaymentRecord);
-// router.patch('/admin/:id', verifySuperAdmin, Users.superAdmin);
+router.patch('/users/:email/verify', _jwt.verifyAdmin, _users["default"].verify);
+router.get('/loans/:id', _jwt.verifyAdmin, _loans["default"].specific);
+router.get('/loans', _jwt.verifyAdmin, _loans["default"].viewLoans);
+router.get('/loans/:id/repayments', _loans["default"].repaymentHistory);
+router.post('/loans', applyForLoan, validationHandler, _jwt.verifyUser, _loans["default"].createLoan);
+router.patch('/loans/:id', _jwt.verifyAdmin, _loans["default"].approve);
+router.post('/loans/:id/repayment', _jwt.verifyAdmin, _loans["default"].createRepayment); // router.patch('/admin/:id', verifySuperAdmin, Users.superAdmin);
 
 var _default = router;
 exports["default"] = _default;
